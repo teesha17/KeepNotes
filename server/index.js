93c12f72f60eda1163e40a9c800258ed083a8bd2
teesha17 <teeshu.2179@gmail.com>
2024-06-28 15:48:08 +0530

    changed filename

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.get('/',(req,res)=>{
 })
 app.use(express.json())
 app.use('/api',require("./Routes/CreateUser"))
-app.use('/api',require("./Routes/OrderData"))
+app.use('/api',require("./Routes/CreateNote"))
 
 
 app.listen(port,()=>
